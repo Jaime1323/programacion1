67c1ae6ed1e9e0e4b4a6633d9f350e6ce32e5cd7
Jaime1323 <jimmypar96@gmail.com>
2016-12-16 10:57:14 -0600

    carpeta2 computo

---
+++ laboratorio2/ejercicio1 carpeta2.py
@@ -0,0 +1,7 @@
+#jaimeparedes mora
+#programa1 carpeta2
+import math
+cat1=input("introdusca cateto1: ")
+cat2=input("introdusca cateto2: ")
+hip=(math.sqrt((cat1*cat1)+(cat2*cat2)))
+print "hipotenusa=",hip
---
+++ laboratorio2/ejercicio2carpeta2.py
@@ -0,0 +1,7 @@
+#jaime paredes mora
+#programa2,carpeta2
+def distancia(km):
+    metros= km/1000
+    bandera=8.74
+    pago=(((metros/250)*1.84)+bandera)
+    return pago
